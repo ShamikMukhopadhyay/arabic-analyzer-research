e126cd367c03c3ab9ab3aa287fc8a45fdb9edc08
“ShamikMukhopadhyay” <shamik.mukhopadhyay@deliveryhero.com>
2024-04-20 11:18:15 +0200

    ES arabic analyzer

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from elasticsearch import Elasticsearch
+
+# Read queries from text file
+with open('queries.txt', 'r', encoding='utf-8') as file:
+    queries = file.read().splitlines()
+
+# Connect to Elasticsearch
+es = Elasticsearch("http://localhost:9200")
+
+def analyze_query(query):
+    body = {
+        "analyzer": "arabic",
+        "text": query
+    }
+    result = es.indices.analyze(body=body)
+    tokens = [token['token'] for token in result['tokens']]
+    return ' '.join(tokens)
+
+results = [analyze_query(query) for query in queries]
+
+df = pd.DataFrame({
+    'Original Query': queries,
+    'Analyzed Query': results
+})
+df.to_csv('analyzed_queries.csv', index=False, encoding='utf-8-sig')
